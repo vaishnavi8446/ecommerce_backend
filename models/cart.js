56a9e97889d5f733c780254022059f4bdbee0285
Vaishnavi Ambolkar <88587534+vaishnavi8446@users.noreply.github.com>
2023-08-08 18:36:37 +0530

    Added cart model

---
+++ models/cart.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { User } = require("./user");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    products: [
+      {
+        productId: Number,
+        title: String,
+        description: String,
+        price: Number,
+        category: String,
+        subCategory: String,
+        discount: Number,
+        availability: String,
+      },
+    ],
+    modifiedOn: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
