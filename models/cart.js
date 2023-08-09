40082f52ddc2b5135b5c545d1e4c56bdd0d19f16
Vaishnavi Ambolkar <88587534+vaishnavi8446@users.noreply.github.com>
2023-08-09 20:52:30 +0530

    Added model cart

--- models/cart.js
+++ models/cart.js
@@ -1,30 +1,59 @@
-const mongoose = require("mongoose");
-const { User } = require("./user");
+const express = require("express");
+const router = express.Router();
+const Cart = require("./../models/cart");
 
-const CartSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    },
-    products: [
-      {
-        productId: Number,
-        title: String,
-        description: String,
-        price: Number,
-        category: String,
-        subCategory: String,
-        discount: Number,
-        availability: String,
-      },
-    ],
-    modifiedOn: {
-      type: Date,
-      default: Date.now
+router.post("/Cart", async (req, res) => {
+  const {
+    productId,
+    title,
+    description,
+    price,
+    category,
+    subCategory,
+    discount,
+  } = req.body;
+
+  const userId = "64d20b9a9a6779f427a2d311"; //log in user id --take userId from logged in user
+  try {
+    let cart = await Cart.findOne({ userId });
+    if (cart) {
+      const newCart = await Cart.create({
+        userId,
+        products: [
+          {
+            productId,
+            title,
+            description,
+            price,
+            category,
+            subCategory,
+            discount,
+          },
+        ],
+      });
+
+      let pRes = newCart.products.push({
+        productId,
+        title,
+        description,
+        price,
+        category,
+        subCategory,
+        discount,
+      });
+
+      return res.status(200).send({
+        msg: "Product added to Cart!",
+        count: pRes,
+        result: newCart,
+      });
     }
-  },
-  { timestamps: true }
-);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error!");
+  }
+});
 
-module.exports = mongoose.model("Cart", CartSchema);
+
+
+module.exports = router;
